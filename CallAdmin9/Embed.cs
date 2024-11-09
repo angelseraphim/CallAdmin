c28630324bf03991afa1f43615e800da429e50c0
AngelSeraphim <server@praniksl.ru>
2024-11-09 22:19:04 +0500

    Exiled 9.0.0

---
+++ CallAdmin9/Embed.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http;
+using System.Text;
+using Exiled.API.Features;
+using Newtonsoft.Json;
+
+namespace CallAdmin
+{
+    public class Embed
+    {
+        public void SendDiscordWebhook(string WebhookURL, string Text, string Title, string WebhookText)
+        {
+            if (WebhookURL.IsEmpty())
+            {
+                Log.Error("Webhook url is empty!");
+                return;
+            }
+            if (Text.IsEmpty())
+            {
+                Log.Error("Text is empty!");
+                return;
+            }
+            if (Title.IsEmpty())
+            {
+                Log.Error("Title is empty!");
+                return;
+            }
+            if (WebhookText.IsEmpty())
+            {
+                Log.Error("WebhookText is empty!");
+                return;
+            }
+            try
+            {
+                var message = new
+                {
+                    content = Text,
+                    embeds = new[]
+    {
+                    new
+                    {
+                        color = 0xff0000,
+                        title = Title,
+                        description = WebhookText,
+                    }
+                }
+                };
+
+                var client = new HttpClient();
+                var json = JsonConvert.SerializeObject(message);
+
+                client.PostAsync(WebhookURL, new StringContent(json, Encoding.UTF8, "application/json"));
+            }
+            catch
+            {
+                Log.Error("Webhook is wrong. Please check your configuration");
+            }
+        }
+    }
+}
---
+++ CallAdmin9/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomPlayerEffects;
+using Exiled.API.Features;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CallAdmin
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Prefix => "CallAdmin";
+        public override string Name => "CallAdmin";
+        public override string Author => "angelseraphim.";
+        public static Plugin plugin;
+        public static Embed embed;
+        public override void OnEnabled()
+        {
+            plugin = this;
+            embed = new Embed();
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            plugin = null;
+            embed = null;
+            base.OnDisabled();
+        }
+    }
+}
